936c241035190239b3a13a1c40fca51d72d67200
Suryaprakash G <141228691+Suryaprakash-G26@users.noreply.github.com>
2023-10-02 15:29:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var resultElement = document.getElementById('result');
+  var buttons = document.querySelectorAll('#calculator button');
+
+  buttons.forEach(function(button) {
+      button.addEventListener('click', function() {
+          var buttonText = button.innerText;
+
+          if (buttonText === 'c') {
+              resultElement.value = '';
+          } else if (buttonText === '=') {
+              try {
+                  resultElement.value = eval(resultElement.value);
+              } catch (error) {
+                  resultElement.value = 'Error';
+              }
+          } else if (buttonText === '√x') {
+             
+              resultElement.value = Math.sqrt(eval(resultElement.value));
+          } else if (buttonText === '%') {
+             
+              resultElement.value = eval(resultElement.value) / 100;
+          } else if (buttonText === '÷') {
+         
+              resultElement.value += '/';
+          } else if (buttonText === '×') {
+              
+              resultElement.value += '*';
+          } else if (buttonText === '-') {
+             
+              resultElement.value += '-';
+          } else {
+              resultElement.value += buttonText;
+          }
+      });
+  });
+});
